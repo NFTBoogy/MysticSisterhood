342f6f96531d2df1db72261e4f22b5b35c009d44
guyett92 <aaronguyett@yahoo.com>
2021-11-03 06:26:29 -0700

    finalizes launch

--- pages/index.js
+++ pages/index.js
@@ -26,6 +26,11 @@ export default function Home() {
         />
       </Head>
 
+      <img
+        className={styles.logo}
+        src="/images/mystic sisterhood logo color.png"
+      />
+
       <main className={styles.main}>
         <h3 className={styles.subtitle}>
           A collection of 7,777 mystically inclined women, all welcoming you
@@ -33,6 +38,17 @@ export default function Home() {
         </h3>
 
         <Fader />
+        <hr
+          className={`${styles.hr} ${styles.animated} ${styles.fadeInUp} ${styles.animatedFadeInUp}`}
+        />
+        <img
+          src="/images/banner.jpg"
+          alt="Mystic Sisters Banner"
+          className={styles.banner}
+        />
+        <hr
+          className={`${styles.hr} ${styles.animated} ${styles.fadeInUp} ${styles.animatedFadeInUp}`}
+        />
 
         <FadeIn className={styles.noteContainer} transitionDuration={1000}>
           <h1 className={styles.leftTitle}>GOOD VIBES ONLY</h1>
@@ -73,16 +89,14 @@ export default function Home() {
         </FadeIn>
       </main>
       <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{" "}
-          <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
-        </a>
+        <span className={styles.logo}>
+          <Image
+            src="/images/mystic sisterhood logo color.png"
+            alt="Vercel Logo"
+            width={800}
+            height={500}
+          />
+        </span>
       </footer>
     </div>
   );
